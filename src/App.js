3aca941953592b6a7daf9cb80f5775230331d1f7
Rajuprajapti <rprajuprajapati16@gmail.com>
2023-04-11 15:50:05 +0530

    add setup done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Note from "./Components/Note";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
